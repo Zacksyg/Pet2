15016080188ab56d7793a793f446433e61085c37
Gabriel <132038804+Zacksyg@users.noreply.github.com>
2024-12-03 17:02:09 -0300

    Delete server directory

--- server/routes/eventRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const eventoController = require('../controllers/eventoController'); 
-const authMiddleware = require('../middleware/auth');
-
-
-router.post('/cadastrar', authMiddleware, eventoController.cadastrarEvento);
-
-module.exports = router;
--- server/routes/protectedRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middleware/auth');
-
-router.get('/protected', auth, (req, res) => {
-    res.send('Acesso autorizado!');
-});
-
-module.exports = router;
